bd23aae7498103224f7733115b1f5ff66bd82cee
asthraksha <asthraksha@gmail.com>
2024-06-01 22:57:02 +0530

    login screen

---
+++ abc_medical_test_company_v2/login.Designer.cs
@@ -0,0 +1,273 @@
+﻿namespace abc_medical_test_company_v2
+{
+    partial class login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel_main = new System.Windows.Forms.Panel();
+            this.panel_header = new System.Windows.Forms.Panel();
+            this.panel_footer = new System.Windows.Forms.Panel();
+            this.panel_left = new System.Windows.Forms.Panel();
+            this.panel_right = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btn_Close = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_Username = new System.Windows.Forms.TextBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_Password = new System.Windows.Forms.TextBox();
+            this.panel_main.SuspendLayout();
+            this.panel_header.SuspendLayout();
+            this.panel_footer.SuspendLayout();
+            this.panel_left.SuspendLayout();
+            this.panel_right.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel_main
+            // 
+            this.panel_main.Controls.Add(this.panel_right);
+            this.panel_main.Controls.Add(this.panel_left);
+            this.panel_main.Controls.Add(this.panel_footer);
+            this.panel_main.Controls.Add(this.panel_header);
+            this.panel_main.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel_main.Location = new System.Drawing.Point(0, 0);
+            this.panel_main.Name = "panel_main";
+            this.panel_main.Size = new System.Drawing.Size(800, 450);
+            this.panel_main.TabIndex = 0;
+            // 
+            // panel_header
+            // 
+            this.panel_header.BackColor = System.Drawing.Color.ForestGreen;
+            this.panel_header.Controls.Add(this.button2);
+            this.panel_header.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel_header.Location = new System.Drawing.Point(0, 0);
+            this.panel_header.Name = "panel_header";
+            this.panel_header.Size = new System.Drawing.Size(800, 50);
+            this.panel_header.TabIndex = 0;
+            // 
+            // panel_footer
+            // 
+            this.panel_footer.BackColor = System.Drawing.Color.ForestGreen;
+            this.panel_footer.Controls.Add(this.btn_Close);
+            this.panel_footer.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel_footer.Location = new System.Drawing.Point(0, 398);
+            this.panel_footer.Name = "panel_footer";
+            this.panel_footer.Size = new System.Drawing.Size(800, 52);
+            this.panel_footer.TabIndex = 1;
+            // 
+            // panel_left
+            // 
+            this.panel_left.BackColor = System.Drawing.Color.ForestGreen;
+            this.panel_left.Controls.Add(this.label3);
+            this.panel_left.Controls.Add(this.pictureBox1);
+            this.panel_left.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel_left.Location = new System.Drawing.Point(0, 50);
+            this.panel_left.Name = "panel_left";
+            this.panel_left.Size = new System.Drawing.Size(351, 348);
+            this.panel_left.TabIndex = 2;
+            // 
+            // panel_right
+            // 
+            this.panel_right.BackColor = System.Drawing.Color.ForestGreen;
+            this.panel_right.Controls.Add(this.button1);
+            this.panel_right.Controls.Add(this.pictureBox2);
+            this.panel_right.Controls.Add(this.txt_Password);
+            this.panel_right.Controls.Add(this.txt_Username);
+            this.panel_right.Controls.Add(this.label2);
+            this.panel_right.Controls.Add(this.label1);
+            this.panel_right.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel_right.Location = new System.Drawing.Point(351, 50);
+            this.panel_right.Name = "panel_right";
+            this.panel_right.Size = new System.Drawing.Size(449, 348);
+            this.panel_right.TabIndex = 3;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::abc_medical_test_company_v2.Properties.Resources.For_You;
+            this.pictureBox1.Location = new System.Drawing.Point(55, 38);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(264, 217);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btn_Close
+            // 
+            this.btn_Close.FlatAppearance.BorderSize = 0;
+            this.btn_Close.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Close.Font = new System.Drawing.Font("DejaVu Sans Condensed", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Close.ForeColor = System.Drawing.Color.White;
+            this.btn_Close.Location = new System.Drawing.Point(704, 8);
+            this.btn_Close.Name = "btn_Close";
+            this.btn_Close.Size = new System.Drawing.Size(75, 32);
+            this.btn_Close.TabIndex = 0;
+            this.btn_Close.Text = "Close";
+            this.btn_Close.UseVisualStyleBackColor = true;
+            this.btn_Close.Click += new System.EventHandler(this.btn_Close_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("DejaVu Sans Condensed", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(101, 158);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Username";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("DejaVu Sans Condensed", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(104, 208);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Password";
+            // 
+            // txt_Username
+            // 
+            this.txt_Username.BackColor = System.Drawing.Color.DarkGreen;
+            this.txt_Username.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_Username.Font = new System.Drawing.Font("DejaVu Sans Condensed", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Username.ForeColor = System.Drawing.Color.White;
+            this.txt_Username.HideSelection = false;
+            this.txt_Username.Location = new System.Drawing.Point(105, 181);
+            this.txt_Username.Name = "txt_Username";
+            this.txt_Username.Size = new System.Drawing.Size(292, 20);
+            this.txt_Username.TabIndex = 1;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::abc_medical_test_company_v2.Properties.Resources.Contacts;
+            this.pictureBox2.Location = new System.Drawing.Point(163, 6);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(158, 136);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 2;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("DejaVu Sans Condensed", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(214, 278);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 32);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btn_Close_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Font = new System.Drawing.Font("DejaVu Sans Condensed", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(704, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(91, 32);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Register";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.btn_Close_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Mistral", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(78, 258);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(214, 41);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "ABC Health Care";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txt_Password
+            // 
+            this.txt_Password.BackColor = System.Drawing.Color.DarkGreen;
+            this.txt_Password.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txt_Password.Font = new System.Drawing.Font("DejaVu Sans Condensed", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_Password.ForeColor = System.Drawing.Color.White;
+            this.txt_Password.HideSelection = false;
+            this.txt_Password.Location = new System.Drawing.Point(105, 231);
+            this.txt_Password.Name = "txt_Password";
+            this.txt_Password.Size = new System.Drawing.Size(292, 20);
+            this.txt_Password.TabIndex = 1;
+            // 
+            // login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel_main);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "login";
+            this.Text = "login";
+            this.panel_main.ResumeLayout(false);
+            this.panel_header.ResumeLayout(false);
+            this.panel_footer.ResumeLayout(false);
+            this.panel_left.ResumeLayout(false);
+            this.panel_left.PerformLayout();
+            this.panel_right.ResumeLayout(false);
+            this.panel_right.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel_main;
+        private System.Windows.Forms.Panel panel_left;
+        private System.Windows.Forms.Panel panel_footer;
+        private System.Windows.Forms.Panel panel_header;
+        private System.Windows.Forms.Panel panel_right;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btn_Close;
+        private System.Windows.Forms.TextBox txt_Username;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_Password;
+    }
+}
---
+++ abc_medical_test_company_v2/login.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace abc_medical_test_company_v2
+{
+    public partial class login : Form
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Close_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
